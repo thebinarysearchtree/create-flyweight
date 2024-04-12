f76487d60d39427c522fa31e025786f9559c3df9
Andrew Jones <thebinarysearchtree@gmail.com>
2024-04-13 08:22:01 +1000

    timestamp

--- migrate.js
+++ migrate.js
@@ -2,11 +2,24 @@ import { database, createMigration, runMigration, makeTypes } from './db.js';
 import readline from 'readline/promises';
 import { stdin as input, stdout as output } from 'process';
 
-const name = process.argv[2];
+const now = () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const hours = String(currentDate.getHours()).padStart(2, '0');
+  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+  const seconds = String(currentDate.getSeconds()).padStart(2, '0');
 
-if (!name) {
-  console.log('Please supply a name for the migration');
-  process.exit();
+  return `${year}${month}${day}${hours}${minutes}${seconds}`;
+};
+
+let name;
+if (process.argv.length > 2) {
+  name = `${now()}_${process.argv[2]}`;
+}
+else {
+  name = now();
 }
 
 let migration;
@@ -25,9 +38,9 @@ if (!migration.sql) {
 console.log(`\n${migration.sql}\n`);
 console.log('Edit the migration file if necessary.');
 const rl = readline.createInterface({ input, output });
-const response = await rl.question('Run migration? (yes)/no:\n');
+const response = await rl.question('Run migration? (y)/n:\n');
 rl.close();
-if (response === 'no') {
+if (response === 'n') {
   try {
     await migration.undo();
   }
@@ -42,6 +55,7 @@ else {
     console.log('Migration ran successfully.');
   }
   catch (e) {
+    await migration.undo();
     console.log('\nMigration rolled back due to:\n');
     throw e;
   }
